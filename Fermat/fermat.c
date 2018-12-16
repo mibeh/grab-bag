3d80d682356109a0bf9385b94e8eb96e1a4a1e6b
Sundown <you@example.com>
2018-12-16 00:10:47 -0500

    initial commit

--- Fermat/fermat.c
+++ Fermat/fermat.c
@@ -1,1 +1,120 @@
+// Author - Michael Ibeh
+// This program finds the prime factors of a number
+// using Fermat's method and Trial division.
 
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+// Checks if a given number is prime
+int is_prime(long long p){
+
+	int i;
+
+	for(i = 2; i <= p / 2; i++)
+		if(p % i == 0)
+			return 0; // not Prime
+
+	return 1; // is prime
+}
+
+// Checks if a number is a perfect square
+int perfect_square(long long n){
+
+	long long rootn = sqrt(n), i;
+
+	if(rootn * rootn == n)
+		return 1;
+
+	return 0;
+}
+
+int main(void){
+	
+	int isPrime;
+	long long i = 0, j, n, x, y, x2, y2;
+	double time;
+	clock_t begin, end;
+
+	while(!feof(stdin)) {
+
+		// Read from stdin
+		scanf("%lld", &n);
+
+		if (i == n)
+			break;
+
+		printf("Trial Division: ");
+
+		begin = clock();
+
+		// Find all Prime factors
+	    for(i=2; i<=sqrt(n); i++) {
+
+	        // Check if 'i' is a factor
+	        if(n%i==0) {
+
+	            // Check if 'i' is a Prime
+	            isPrime = is_prime(i);
+
+							// J is the other factor
+							j = n / i;
+
+							// Check if 'j' is a Prime
+							isPrime = is_prime(i);
+
+	            // If 'i' and 'j' are prime then we're good
+	            if(isPrime==1) {
+									end = clock();
+									time = (double)(end - begin) / CLOCKS_PER_SEC;
+	              	// printf ("%lld*%lld=%lld\n", i, j, n);
+									printf("The factors of %lld are %lld and %lld. This took %lf seconds\n", n, i, j, time);
+									break;
+	            }
+	        }
+					else {
+						// if the factors are 1 and itself
+						if(i == (int)sqrt(n)){
+							end = clock();
+							time = (double)(end - begin) / CLOCKS_PER_SEC;
+							printf("The factors of %lld are 1 and %lld. This took %lf seconds\n", n, n, time);
+						}
+					}
+	    }
+
+		printf("Fermat:\t\t");
+
+		// One of the factors must be at least as great as the square root of n
+		x = ceil(sqrt(n));
+
+		begin = clock();
+
+		while(1){
+
+			// Setup fermat formula
+			x2 = x * x;
+			y2 = x2 - n;
+
+			// IF it is a perfect square
+			if(perfect_square(y2)){
+
+				y = sqrt(y2);
+
+				end = clock();
+				time = (double)(end - begin) / CLOCKS_PER_SEC;
+
+				printf("The factors of %lld are %lld and %lld. This took %lf seconds\n", n, (x-y), (x+y), time);
+				fflush(stdout);
+
+				break;
+			}
+			// Try next x value
+			x++;
+		}
+
+		// Don't reread last value
+		i = n;
+	}
+
+	return 0;
+}
