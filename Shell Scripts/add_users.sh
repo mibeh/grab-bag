12073915c9dffacd3b86f9b32cf33e2e81b94433
mibeh <mibeh18@gmail.com>
2019-01-14 14:45:42 -0500

    Added more secure password gen

--- Shell Scripts/add_users.sh
+++ Shell Scripts/add_users.sh
@@ -3,9 +3,9 @@
 USERNAMES=$(cat users)
 
 for u in $USERNAMES; do
-        echo "$u"
         useradd -m -s "/bin/bash" -d /home/"$u" "$u"
-        echo "$u":"$u"123 | sudo chpasswd
-        echo user "$u" added with password "$u"123
+        p=$(date | md5sum | cut -b1-20)
+	echo "$u":"$u""$p" | sudo chpasswd
+        echo user "$u" added with password "$p"
         chage -d 0 $u
 done
